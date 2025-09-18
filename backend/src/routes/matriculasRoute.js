e36eb1f26e10bd15b86dab6682eed24658bc9e22
JoseEduardo-max <jeduardoff2@gmail.com>
2025-09-18 12:20:09 -0300

    refatoração do codigo

--- backend/src/routes/matriculasRoute.js
+++ backend/src/routes/matriculasRoute.js
@@ -3,16 +3,10 @@ const router = express.Router();
 const MatriculaController = require('../controllers/matriculasController');
 
 router.post('/', MatriculaController.MatriculaCreate);
-
-
 router.get('/', MatriculaController.MatriculaList);
-
-
 router.get('/:id', MatriculaController.MatriculaListId);
-
 router.put('/:id', MatriculaController.MatriculaUpdate);
-
-
 router.delete('/:id', MatriculaController.MatriculaDelete);
+
 
 module.exports = router;
--- backend/src/routes/usuariosRoute.js
+++ backend/src/routes/usuariosRoute.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router()
 const UsersController = require('../controllers/usuarioController')
 
-
-
 router.post('/', UsersController.UsersCreate)
 router.get('/', UsersController.UsersList)
 router.get('/:id',UsersController.UsersListId)
